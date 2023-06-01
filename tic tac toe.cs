f825ac319173d447b3a8c11eefbc3039ae3f38bf
konny63 <92335349+konny63@users.noreply.github.com>
2023-06-02 01:00:38 +0200

    tic tac toe.cs

--- tic tac toe.cs
+++ tic tac toe.cs
@@ -4,38 +4,42 @@ namespace TIC_TAC_TOE
 {
     class Program
     {
-        //making array and
-        //by default I am providing 0-9 where no use of zero
+        //skapa en array (för brädet)
+        //med hjälp av 0-9 utan att använda 0 pga spelets karaktärer
         static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        static int player = 1; //By default player 1 is set
-        static int choice; //This holds the choice at which position user want to mark
-        // The flag variable checks who has won if it's value is 1 then someone has won the match
-        //if -1 then Match has Draw if 0 then match is still running
+        static int player = 1; //sätter 1 som standard
+        static int choice; //håller vilken ruta såelaren vill markera med sitt täcken
+        // "Flag" variaben kollar om den är lika med 1 och då har någon vunnit
+        //om det är -1 är det oavgjort och är det 0 är matchen fortfarande igång
         static int flag = 0;
         static void Main(string[] args)
         {
             do
             {
-                Console.Clear();// whenever loop will be again start then screen will be clear
+                Console.Clear();// när spelet startar igen töms konsolen 
                 Console.WriteLine("Player1:X and Player2:O");
                 Console.WriteLine("\n");
-                if (player % 2 == 0)//checking the chance of the player
+                if (player % 2 == 0)//kollar vems tur det är
                 {
-                    Console.WriteLine("Player 2 Chance");
+                    Console.WriteLine("spelare 2s tur");
                 }
                 else
                 {
-                    Console.WriteLine("Player 1 Chance");
+                    Console.WriteLine("spelare 1s tur");
                 }
                 Console.WriteLine("\n");
-                Board();// calling the board Function
-                choice = int.Parse(Console.ReadLine());//Taking users choice
-                // checking that position where user want to run is marked (with X or O) or not
-                if (arr[choice] != 'X' && arr[choice] != 'O')
+                Board();// Bestämmer funktionen för brädet
+                Console.WriteLine("\n");
+                Console.WriteLine("välj en ruta att markera tack");
+                choice = int.Parse(Console.ReadLine());//läser spelarens val
+
+                // Bestämmer vilken figur som rutan markeras med beroende på vems tur det är
+                if (arr[choice] != 'X' && arr[choice] != '@')
                 {
-                    if (player % 2 == 0) //if chance is of player 2 then mark O else mark X
+                    //om spelare 2s tur markera med @ annars med X
+                    if (player % 2 == 0) 
                     {
-                        arr[choice] = 'O';
+                        arr[choice] = '@';
                         player++;
                     }
                     else
@@ -45,34 +49,34 @@ namespace TIC_TAC_TOE
                     }
                 }
                 else
-                //If there is any possition where user want to run
-                //and that is already marked then show message and load board again
+                //Om den valda rutan är upptagen visa texten och ladda om brädet utan att byta till nästa spelares tur.
                 {
-                    Console.WriteLine("Sorry the row {0} is already marked with {1}", choice, arr[choice]);
+                    Console.WriteLine("Ruta nummer {0} är redan markerad med {1}", choice, arr[choice]);
                     Console.WriteLine("\n");
-                    Console.WriteLine("Please wait 2 second board is loading again.....");
+                    Console.WriteLine("vänta 2 sekunder medan brädet laddas in igen.....");
                     Thread.Sleep(2000);
                 }
-                flag = CheckWin();// calling of check win
+                flag = CheckWin();// nämner funktionen som kollar om nån vunnit
             }
             while (flag != 1 && flag != -1);
-            // This loop will be run until all cell of the grid is not marked
-            //with X and O or some player is not win
-            Console.Clear();// clearing the console
-            Board();// getting filled board again
+            // loop som körs tills någon har vunnit eller att det har blivit oavgjort (brädet är fullt)
+            Console.Clear();// töm konsolen
+            Board();// fyll brädet med siffror igen
             if (flag == 1)
-            // if flag value is 1 then someone has win or
-            //means who played marked last time which has win
+            // om "Flag" är lika med 1 har någon vunnit
+            //alltså personen som markerade en ruta senast har vunnit
             {
+                Console.WriteLine("\n");
                 Console.WriteLine("Player {0} has won", (player % 2) + 1);
             }
-            else// if flag value is -1 the match will be draw and no one is winner
+            else// Om "Flag" är lika med -1 är det oavgjort och ingen har vunnit
             {
+                Console.WriteLine("\n");
                 Console.WriteLine("Draw");
             }
-            Console.ReadLine();
+            Environment.Exit(0); 
         }
-        // Board method which creats board
+        // Bräde metoden som skapar brädet
         private static void Board()
         {
             Console.WriteLine("     |     |      ");
@@ -88,41 +92,41 @@ namespace TIC_TAC_TOE
         //Checking that any player has won or not
         private static int CheckWin()
         {
-            #region Horzontal Winning Condtion
-            //Winning Condition For First Row
+            #region Horisontala vinnande möjliga utfall 
+            //vinnande alternativ för första raden
             if (arr[1] == arr[2] && arr[2] == arr[3])
             {
                 return 1;
             }
-            //Winning Condition For Second Row
+            //vinnande möjliga utfall för andra raden
             else if (arr[4] == arr[5] && arr[5] == arr[6])
             {
                 return 1;
             }
-            //Winning Condition For Third Row
-            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            //vinnande möjliga utfall för tredje raden
+            else if (arr[7] == arr[8] && arr[8] == arr[9])
             {
                 return 1;
             }
             #endregion
-            #region vertical Winning Condtion
-            //Winning Condition For First Column
+            #region vertikala vinnande möjliga utfall
+            //vinnande möjliga utfall för första raden
             else if (arr[1] == arr[4] && arr[4] == arr[7])
             {
                 return 1;
             }
-            //Winning Condition For Second Column
+            //vinnande möjliga utfall för andra raden
             else if (arr[2] == arr[5] && arr[5] == arr[8])
             {
                 return 1;
             }
-            //Winning Condition For Third Column
+            //vinnande möjliga utfall för tredje raden
             else if (arr[3] == arr[6] && arr[6] == arr[9])
             {
                 return 1;
             }
             #endregion
-            #region Diagonal Winning Condition
+            #region Diagonala vinnande möjliga utfall
             else if (arr[1] == arr[5] && arr[5] == arr[9])
             {
                 return 1;
@@ -133,7 +137,7 @@ namespace TIC_TAC_TOE
             }
             #endregion
             #region Checking For Draw
-            // If all the cells or values filled with X or O then any player has won the match
+            // Om alla rutorna är markerade och ingen har vunnit så är matchen avslutad som oavgjord
             else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
             {
                 return -1;
